7a8e0096747441b0d1efa5a40d0eb2f7176c6113
MaksHafner <maks.hafner@t-2.net>
2017-06-14 14:03:17 +0200

    Initial commit

---
+++ REST/api/models/todoModel.js
@@ -0,0 +1,25 @@
+﻿
+'use strict';
+var influx = require('influx');
+var Schema = influx.Schema;
+
+
+var TaskSchema = new Schema({
+	name: {
+		type: String,
+		Required: 'Kindly enter the name of the task'
+	},
+	Created_date: {
+		type: Date,
+		default: Date.now
+	},
+	status: {
+		type: [{
+			type: String,
+			enum: ['pending', 'ongoing', 'completed']
+		}],
+		default: ['pending']
+	}
+});
+
+module.exports = influx.model('Tasks', TaskSchema)
