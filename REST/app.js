5d609674c188a06c559c6694c8acc7ec8d2f738b
MaksHafner <maks.hafner@t-2.net>
2017-06-15 08:20:20 +0200

    Added influxdb connection

--- REST/app.js
+++ REST/app.js
@@ -1,8 +1,72 @@
-'use strict';
-var express = require('express'),
-	app = express(),
-	port = process.env.PORT || 3000;
+const Influx = require('influx')
+const express = require('express')
+const http = require('http')
+const os = require('os')
 
-app.listen(port);
+const app = express()
 
-console.log('todo list RESTful API server started on: ' + port);
+const influx = new Influx.InfluxDB({
+	host: 'localhost',
+	database: 'letni1',
+	schema: [
+		{
+			measurement: 'metering',
+			fields: {
+				path: Influx.FieldType.STRING,
+				duration: Influx.FieldType.INTEGER
+			},
+			tags: [
+				'host'
+			]
+		}
+	]
+})
+
+influx.getDatabaseNames()
+	.then(names => {
+		if (!names.includes('letni1')) {
+			return influx.createDatabase('letni1');
+		}
+	})
+	.then(() => {
+		http.createServer(app).listen(3000, function () {
+			console.log('Listening on port 3000')
+		})
+	})
+	.catch(err => {
+		console.error(`Error creating Influx database!`);
+	})
+
+app.use((req, res, next) => {
+	const start = Date.now()
+
+	res.on('finish', () => {
+		const duration = Date.now() - start
+		console.log(`Request to ${req.path} took ${duration}ms`);
+
+		influx.writePoints([
+			{
+				measurement: 'metering',
+				tags: { host: os.hostname() },
+				fields: { duration, path: req.path },
+			}
+		]).catch(err => {
+			console.error(`Error saving data to InfluxDB! ${err.stack}`)
+		})
+	})
+	return next()
+})
+
+//app.get('/', function (req, res) {
+//	setTimeout(() => res.end('Hello world!'), Math.random() * 500)
+//})
+
+app.get('/times', function (req, res) {
+	influx.query(`
+    select * from metering
+  `).then(result => {
+			res.json(result)
+		}).catch(err => {
+			res.status(500).send(err.stack)
+		})
+})
